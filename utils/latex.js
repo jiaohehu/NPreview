4cdb70d4557707935c272764d06ebbee0839ace5
jiaohehu <jiaohe8033790290@163.com>
2020-03-18 23:11:02 -0400

    Updated

--- utils/latex.js
+++ utils/latex.js
@@ -1,5 +1,5 @@
 const { NitrilePreviewNode } = require ('/Users/james/github/nitrile-preview/lib/nitrile-preview-node'); 
-new NitrilePreviewNode().toLatex('/Users/james/github/nitrile-preview/utils/test-dlst.md')
+new NitrilePreviewNode().toLatex('/Users/james/github/nitrile-preview/utils/test-dt.md')
      .then(f => console.log(f)).catch(err => console.log(err));
 
 

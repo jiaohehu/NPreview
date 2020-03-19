576f44d71819a1312d17fa0285d294c5a8b3c620
jiaohehu <jiaohe8033790290@163.com>
2020-03-18 21:40:53 -0400

    Changed so that it always switches the editor for NitrilePreview

---
+++ utils/latex.js
@@ -0,0 +1,5 @@
+const { NitrilePreviewNode } = require ('/Users/james/github/nitrile-preview/lib/nitrile-preview-node'); 
+new NitrilePreviewNode().toLatex('/Users/james/github/nitrile-preview/utils/test-dlst.md')
+     .then(f => console.log(f)).catch(err => console.log(err));
+
+

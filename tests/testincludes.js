92eaf46287ce978ba859b949715ec6427b739b05
jiaohehu <jiaohe8033790290@163.com>
2020-03-29 00:03:31 -0400

    UPdated.

--- tests/testincludes.js
+++ tests/testincludes.js
@@ -1,21 +1,31 @@
 const { NitrilePreviewLatex } = require('../lib/nitrile-preview-latex');
+const { NitrilePreviewHtml } = require('../lib/nitrile-preview-html');
 const utils = require('../lib/nitrile-preview-utils');
+const path = require('path');
 
 console.log(process.argv);
 const fname = process.argv[2];
 console.log(fname);
+const parser = new NitrilePreviewLatex();
 
-utils.readFileAsync(fname).then(
-     out => {
-          const parser = new NitrilePreviewLatex();
-          const [main, config] = parser.toFlow(out.split('\n'), 0, fname);
-          const all = parser.idenBlocks(main, config);
-          console.log(main);
-          console.log(config);
-          var o = parser.translateLatex(main,config);
-          var out = o.join('\n');
-          console.log(out);
-          var subs = parser.toSubs(main);
-          console.log(subs);
+var work = async () => {
+  let out = await utils.readFileAsync(fname);
+  var [main, config] = parser.toFlow(out.split('\n'), 0, fname);
+  var subs = parser.toSubs(main);
+  for( var sub of subs ) {
+    var fsubfname = path.join(path.dirname(fname),sub.subfname);
+    sub.out = await utils.readFileAsync(fsubfname);
+    const [flow] = parser.toFlow(sub.out.split('\n'),sub.sublevel,sub.subfname);
+    sub.flow = flow;
+  }
+  main = parser.insertSubs(main,subs);
+  parser.idenBlocks(main, config);
+  parser.translateBlocks(main,config);
+  var texlines = main.map(x => x.latex);
+  console.log(texlines);
+  return texlines;
+};
           
-     });
+work().then( texlines => {
+
+});

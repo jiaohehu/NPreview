2bef94f0cfdac0533a098be21b1262eac13bbf53
jiaohehu <jiaohe8033790290@163.com>
2020-03-28 20:40:45 -0400

    More fixes for new block style.

--- tests/testepub.js
+++ tests/testepub.js
@@ -10,19 +10,19 @@ console.log(fname);
 utils.readFileAsync(fname).then(
      out => {
           const parser = new NitrilePreviewHtml();
+          parser.isepub = 1;
           const [main, flags] = parser.toFlow(out.split('\n'), 0, fname);
           const config = parser.toConfig(flags);
           const all = parser.idenBlocks(main,config);
           console.log(all);
           console.log(config);
-          var htmls = parser.translateHtml(main,config);
-          var chaps = parser.chaps;
+          parser.translateHtml(main,config);
           let title = config.ALL.title ? config.ALL.title : 'Untitled';
           let author = config.ALL.author ? config.ALL.author : '';
           var epub = new NitrilePreviewEpub();
           var imagemap = new Map();///empty imagemap
           var dirname = path.dirname(fname);
-          epub.generateAsync(parser,parser.escape(title),parser.escape(author),htmls,chaps,imagemap,config,dirname).then( x => {
+          epub.generateAsync(parser,parser.escape(title),parser.escape(author),all,imagemap,config,dirname).then( x => {
             console.log('finished');
           }).catch( x => console.log(x) );
           

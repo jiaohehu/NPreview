f54bed79d1bd4235dbe067a1070042341b6e7a10
jiaohehu <jiaohe8033790290@163.com>
2020-04-07 03:42:39 -0400

    Updated.

--- tests/testhtml.js
+++ tests/testhtml.js
@@ -7,10 +7,12 @@ console.log(fname);
 
 utils.readFileAsync(fname).then(
      out => {
+          var lines = out.split('\n');
           const parser = new NitrilePreviewHtml();
-          const [main, config] = parser.toFlow(out.split('\n'), fname);
-          parser.idenBlocks(main,config);
-          parser.translateBlocks(main,config);
+          parser.readFromLines(lines,fname);
+          parser.idenBlocks();
+          parser.translateBlocks();
+          var main = parser.blocks;
           var htmls = main.map(x=>x.html);
           console.log(htmls.join('\n'));
      });

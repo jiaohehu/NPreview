5712bcfe3c32974866437b9aeb9edcdb0a6ee456
jiaohehu <jiaohe8033790290@163.com>
2020-04-13 14:28:51 -0400

    Added toNots() to combine \not with \equiv and others.

--- tests/testhtml.js
+++ tests/testhtml.js
@@ -3,16 +3,18 @@ const utils = require('../lib/nitrile-preview-utils');
 
 console.log(process.argv);
 const fname = process.argv[2];
-console.log(fname);
 
-utils.readFileAsync(fname).then(
-     out => {
-          var lines = out.split('\n');
-          const parser = new NitrilePreviewHtml();
-          parser.readFromLines(lines,fname);
-          parser.idenBlocks();
-          parser.translateBlocks();
-          var main = parser.blocks;
-          var htmls = main.map(x=>x.html);
-          console.log(htmls.join('\n'));
-     });
+var work = async ()=>{
+  console.log(fname);
+  var out = await utils.readFileAsync(fname);
+  var lines = out.split('\n');
+  const parser = new NitrilePreviewHtml();
+  parser.readFromLines(lines);
+  parser.idenBlocks();
+  parser.translateBlocks();
+  var main = parser.blocks;
+  var htmls = main.map(x=>x.html);
+  console.log(htmls.join('\n'));
+};
+
+work();

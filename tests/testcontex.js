85b4beca4652998bb9afe761f5ac833be9812d2b
jiaohehu <jiaohe8033790290@163.com>
2020-05-30 08:19:14 -0400

    Updated.

--- tests/testcontex.js
+++ tests/testcontex.js
@@ -5,13 +5,18 @@ console.log(process.argv);
 const fname = process.argv[2];
 console.log(fname);
 
-utils.readFileAsync(fname).then(
-     out => {
-          const parser = new NitrilePreviewContex();
-          parser.readFromFileAsync(fname);
-          parser.idenBlocks();
-          parser.translateBlocks();
-          var main = parser.blocks;
-          var texlines = main.map(x=>x.latex);
-          console.log(texlines.join('\n'));
-     });
+var work = async ()=>{
+  console.log(fname);
+  var out = await utils.readFileAsync(fname);
+  var lines = out.split('\n');
+  console.log(lines);
+  const parser = new NitrilePreviewContex();
+  parser.readFromLines(lines);
+  parser.idenBlocks();
+  parser.translateBlocks();
+  var main = parser.blocks;
+  var outlines = main.map(x=>x.latex);
+  console.log(outlines.join('\n'));
+};
+
+work();

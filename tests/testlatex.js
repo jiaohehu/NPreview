e57fe06b8710f20d3e78503daaadd54de1cc41f8
jiaohehu <jiaohe8033790290@163.com>
2020-05-30 09:16:26 -0400

    Updated to improve fontifyLATEX().

--- tests/testlatex.js
+++ tests/testlatex.js
@@ -5,12 +5,19 @@ console.log(process.argv);
 const fname = process.argv[2];
 console.log(fname);
 
-utils.readFileAsync(fname).then(
-     out => {
-          const parser = new NitrilePreviewLatex();
-          const [main, config] = parser.toFlow(out.split('\n'),fname);
-          parser.idenBlocks(main,config);
-          parser.translateBlocks(main,config);
-          var texlines = main.map(x=>x.latex);
-          console.log(texlines.join('\n'));
-     });
+var work = async ()=>{
+  console.log(fname);
+  var out = await utils.readFileAsync(fname);
+  var lines = out.split('\n');
+  console.log(lines);
+  const parser = new NitrilePreviewLatex();
+  parser.readFromLines(lines);
+  parser.idenBlocks();
+  parser.translateBlocks();
+  var main = parser.blocks;
+  var outlines = main.map(x=>x.latex);
+  console.log(outlines.join('\n'));
+};
+
+work();
+

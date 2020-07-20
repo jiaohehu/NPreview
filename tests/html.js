b81b5fb18da624e053c322ff6135c038e54d496c
jiaohehu <jiaohe8033790290@163.com>
2020-07-20 02:42:50 -0400

    More works on SVG diagram fixes

--- tests/html.js
+++ tests/html.js
@@ -1,3 +1,4 @@
+const { NitrilePreviewParser } = require('../lib/nitrile-preview-parser');
 const { NitrilePreviewHtml } = require('../lib/nitrile-preview-html');
 const utils = require('../lib/nitrile-preview-utils');
 
@@ -8,10 +9,11 @@ var work = async ()=>{
   console.log(fname);
   var out = await utils.readFileAsync(fname);
   var lines = out.split('\n');
-  const parser = new NitrilePreviewHtml();
-  parser.readFromLines(lines);
+  const parser = new NitrilePreviewParser();
+  parser.read_md_lines(lines);
   parser.iden_blocks();
-  parser.translate_blocks();
+  var translator = new NitrilePreviewHtml();
+  parser.translate_blocks(translator);
   var main = parser.blocks;
   var htmls = main.map(x=>x.html);
   console.log(htmls.join('\n'));

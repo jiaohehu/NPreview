1eb5543a17c0df455fff6e899aff156d1d5de220
jiaohehu <jiaohe8033790290@163.com>
2020-08-24 15:23:07 -0400

    Update beamer.js

--- tests/beamer.js
+++ tests/beamer.js
@@ -2,22 +2,25 @@ const { NitrilePreviewParser } = require('../lib/nitrile-preview-parser');
 const { NitrilePreviewBeamer } = require('../lib/nitrile-preview-beamer');
 const utils = require('../lib/nitrile-preview-utils');
 
+var work = async ()=>{
+  var out = await utils.read_file_async(fname);
+  var lines = out.split('\n');
+  const parser = new NitrilePreviewParser();
+  const translator = new NitrilePreviewBeamer(parser);
+  parser.read_md_lines(lines);
+  translator.translate_blocks();
+  var data = translator.to_beamer_document();
+  return(data);
+};
+
 console.log(process.argv);
 const fname = process.argv[2];
 console.log(fname);
 
-var work = async ()=>{
-  console.log(fname);
-  const parser = new NitrilePreviewParser();
-  const translator = new NitrilePreviewBeamer();
-  await parser.read_md_file_async(fname);
-  parser.translate_blocks(translator);
-  var main = parser.blocks;
-  var latex = main.map(x=>x.latex);
-  console.log(latex.join('\n'));
-  var data = translator.to_beamer_document();
-  console.log(data);
-};
-
-work();
+if (fname) {
+  work().then(x => console.log(x));
+  setTimeout(function () { }, 1000);
+}else{
+  console.log("Empty file name");
+}
 
